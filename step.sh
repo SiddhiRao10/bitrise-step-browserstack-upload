4bae44931a26fb74fa57beecbdf5cd8b6f41640c
SiddhiRao10 <107387552+SiddhiRao10@users.noreply.github.com>
2022-09-02 00:24:25 +0530

    Update step.sh

--- step.sh
+++ step.sh
@@ -3,3 +3,12 @@ set -e
 set -x
 set -o pipefail
 curl -u "$browserstack_username:$browserstack_access_key" -X POST https://api-cloud.browserstack.com/app-automate/upload -F "file=@$upload_path" -F 'data={"custom_id": "'$custom_id'"}' | jq -j '.app_url' | envman add --key BROWSERSTACK_APP_URL
+echo $BROWSERSTACK_APP_URL
+git clone https://github.com/SiddhiRao10/python-appium-app-browserstack.git
+cd bstack-robot-framework
+pip install robotframework
+pip install --upgrade robotframework-seleniumlibrary
+pip install robotframework-pabot
+pip install robotframework-appiumlibrary
+cd app/test
+robot Appium_android.robot
